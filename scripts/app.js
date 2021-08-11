fb8b00dfa23afd2f455ebba399870efabb4bbd7f
Julia Khayrulina <julia748@mail.ru>
2021-08-11 23:33:29 +0300

    Implement todo sheet:
    1) adding new cases by pressing ENTER and on the PLUS button
    2) mark the completed cases, the completed cases must be moved to the block with the completed cases
    3) empty cases should not be added
    4) the input field after adding the case should be cleared
    5) delete cases on the CART button
    6) save case data in localStorage
    7) cases from localStorage should be loaded automatically when the page loads

---
+++ scripts/app.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control'),
+  headerInput = document.querySelector('.header-input'),
+  todoList = document.querySelector('.todo-list'),
+  todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = [];
+
+render();
+
+function render() {
+  todoList.textContent = '';
+  todoCompleted.textContent = '';
+
+  todoData = JSON.parse(localStorage.getItem('todo'));
+
+  todoData.forEach((item) => {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+
+    li.innerHTML = `
+    <span class="text-todo">${item.value}</span>
+    <div class="todo-buttons">
+      <button class="todo-remove"></button>
+      <button class="todo-complete"></button>
+    </div>
+    `;
+
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+    const btnTodoComplete = li.querySelector('.todo-complete');
+    btnTodoComplete.addEventListener('click', function () {
+      item.completed = !item.completed;
+      localStorage.setItem('todo', JSON.stringify(todoData));
+      render();
+    });
+
+    const btnTodoRemove = li.querySelector('.todo-remove');
+    btnTodoRemove.addEventListener('click', function () {
+      todoData = todoData.filter((items) => items !== item);
+      localStorage.setItem('todo', JSON.stringify(todoData));
+      render();
+    });
+  });
+}
+
+todoControl.addEventListener('submit', function (event) {
+  event.preventDefault();
+  headerInput.value = headerInput.value.trim();
+  if (headerInput.value) {
+    todoData.push({
+      value: headerInput.value,
+      completed: false,
+    });
+    localStorage.setItem('todo', JSON.stringify(todoData));
+    headerInput.value = '';
+    render();
+  }
+});
