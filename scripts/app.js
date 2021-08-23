1cd80691b280ee8f13c3ac5d6125146d854cb0a5
Julia Khayrulina <julia748@mail.ru>
2021-08-24 01:36:40 +0300

    fix: write a check for the presence of data in the locale storage

--- scripts/app.js
+++ scripts/app.js
@@ -12,46 +12,49 @@ render();
 function render() {
   todoList.textContent = '';
   todoCompleted.textContent = '';
+  let data = JSON.parse(localStorage.getItem('todo'));
 
-  todoData = JSON.parse(localStorage.getItem('todo'));
+  if (data) {
+    todoData = data;
+    todoData.forEach((item) => {
+      const li = document.createElement('li');
+      li.classList.add('todo-item');
 
-  todoData.forEach((item) => {
-    const li = document.createElement('li');
-    li.classList.add('todo-item');
+      li.innerHTML = `
+      <span class="text-todo">${item.value}</span>
+      <div class="todo-buttons">
+        <button class="todo-remove"></button>
+        <button class="todo-complete"></button>
+      </div>
+      `;
 
-    li.innerHTML = `
-    <span class="text-todo">${item.value}</span>
-    <div class="todo-buttons">
-      <button class="todo-remove"></button>
-      <button class="todo-complete"></button>
-    </div>
-    `;
+      if (item.completed) {
+        todoCompleted.append(li);
+      } else {
+        todoList.append(li);
+      }
 
-    if (item.completed) {
-      todoCompleted.append(li);
-    } else {
-      todoList.append(li);
-    }
+      const btnTodoComplete = li.querySelector('.todo-complete');
+      btnTodoComplete.addEventListener('click', function () {
+        item.completed = !item.completed;
+        localStorage.setItem('todo', JSON.stringify(todoData));
+        render();
+      });
 
-    const btnTodoComplete = li.querySelector('.todo-complete');
-    btnTodoComplete.addEventListener('click', function () {
-      item.completed = !item.completed;
-      localStorage.setItem('todo', JSON.stringify(todoData));
-      render();
+      const btnTodoRemove = li.querySelector('.todo-remove');
+      btnTodoRemove.addEventListener('click', function () {
+        todoData = todoData.filter((items) => items !== item);
+        localStorage.setItem('todo', JSON.stringify(todoData));
+        render();
+      });
     });
-
-    const btnTodoRemove = li.querySelector('.todo-remove');
-    btnTodoRemove.addEventListener('click', function () {
-      todoData = todoData.filter((items) => items !== item);
-      localStorage.setItem('todo', JSON.stringify(todoData));
-      render();
-    });
-  });
+  }
 }
 
 todoControl.addEventListener('submit', function (event) {
   event.preventDefault();
   headerInput.value = headerInput.value.trim();
+  console.log(todoData);
   if (headerInput.value) {
     todoData.push({
       value: headerInput.value,
